fb52ae38080d02497ed4fb8017dd2d3892922af5
yoshivb <yoshivanbelkom@msn.com>
2024-07-09 11:49:01 +0200

    Fix title entry

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -3,12 +3,17 @@ definePageMeta({
   layout: 'docs'
 })
 const route = useRoute()
-const { data: page } = await useAsyncData(`docs-${route.path}`, () => queryContent(route.path).findOne())
+const { data: page } = await useAsyncData(`docs-${route.path}`, () => queryContent(route.path).findOne());
 
 if (!page.value) { 
-    throw createError({ statusCode: 404, statusMessage: 'Page not found' }) 
+    throw createError({ statusCode: 404, statusMessage: 'Page not found' });
 }
 
+useContentHead({
+    head: {
+        titleTemplate: () => `${page.value?.title} | P4nth3rworld Wiki`,
+    }
+});
 </script>
 
 <template>
